5033fe4a0a33c2944af34b2d6790bf0b9dbd6025
Chase (Ryan Cline) <ryancline88@yahoo.com>
2017-09-13 13:35:39 -0600

    Update

--- public/events.js
+++ public/events.js
@@ -1,6 +1,13 @@
 var button = document.getElementById('begin')
 var header = document.getElementsByTagName('header')[0]
 var main = document.getElementsByTagName('main')[0]
+var scene_5c = []
+var scene_5b = []
+var scene_5a = []
+var scene_4 = []
+var scene_3c = []
+var scene_3b = []
+var scene_3a = []
 var scene_2b = []
 var scene_2a = []
 var scene_1c = []
@@ -9,24 +16,36 @@ var scene_1a = []
 var scene_0 = []
 
 // Scenes
-var sceneLibrary = {scene_0: []}
 
 var currScene = ''
 
-var scene_2b = ['> You offer to work with the troll to find a way out\n> The troll is pleased and gives you a crushing hug that leaves you gasping for air', 'none', 'none', 'none']
+var scene_5c = ['Daggers text', 'none', 'none', 'none']
 
-var scene_2a = ['> The troll rips you apart limb from limb\n> Bad move\n\nPress enter to return to previous scene', 'none', 'none', 'none']
+var scene_5b = ['Staff text', 'none', 'none', 'none']
 
-var scene_1c = ['> The troll stirs, grumbles, and bellows "Why have you disturbed my sleep?!"\n\nPress a number:\n1. Attack troll\n2. Attempt to make friends', scene_2a, scene_2b, 'none']
+var scene_5a = ['Axe text', 'none', 'none', 'none']
 
-var scene_1aa = ['> The gate is locked.\n\nPress a number:\n1. Look around the cell\n2. Attempt to wake up troll', [], scene_1c, 'none']
+var scene_4 = ['> You head down a corridor and enter a torch-lit room.\n> You see a table with an axe, a magic staff, and a pair of daggers.\n> Which weapon do you choose?\n\nPress a number:\n1. Axe - Barbarian\n2. Staff - Mage\n3. Daggers - Rogue', scene_5a, scene_5b, scene_5c]
 
-var scene_1b = ['> The cell is small and empty except for your noisy cellmate.\n> There\'s nothing you can use here.\n\nPress a number:\n1. Check gate\n2. Attempt to wake up the troll', scene_1aa, scene_1c, 'none']
+var scene_3c = ['> Grol shakes the ground as he jumps up and down with excitement\n> Grol: "Yes! Grol be free of masters! All Grol want is munch time on rocks and quiet for sleep time. Grol no ask for much."\n> The rock troll steps out of the way and lets you out of the cell.\n\nPress enter to continue...', scene_4, 'none', 'none', 'continue']
 
-var scene_1a = ['> The gate is locked.\n\nPress a number:\n1. Look around the cell\n2. Attempt to wake up troll', scene_1b, scene_1c, 'none']
+var scene_3b = ['(Failed)\n> Grol: "Grol is not caring. Masters already know Grol sleeps all day. Go tell masters. Grol gets beat either way."\n\nPress enter to return to last checkpoint', 'none', 'none', 'none']
 
-var scene_0 = ['> You find yourself in a cold dark cell.\n> Next to you, a rock troll fills the room with deafening snores.\n> What do you do?\n\nPress a number:\n1. Check gate\n2. Look around the cell\n3. Attempt to wake up the troll', scene_1a, scene_1b, scene_1c]
+var scene_3a = ['> You: "Tell me more about your masters."\n> Grol: "Masters powerful with magic. Take blood from stinky humans they do. Scary angry bitey dogs they make using magic from man-blood. Grol no like scary masters pets. Bitey dogs bark and snap at poor Grol"\n\nPress a number:\n1. Threaten to tell on Grol for sleeping (Intimidate)\n2. Offer your help with getting rid of Grol\'s masters (Persuade)', scene_3b, scene_3c, 'none']
 
+var scene_2b = ['> You: "Are you in charge here?"\n> Grol: "Grol work for masters. Masters beat Grol for too much sleep, make Grol watch over stinky humans"\n\nPress a number:\n1. Ask for more info on Grol\'s masters\n2. Threaten to tell on Grol for sleeping (Intimidate)\n3. Offer your help with getting rid of Grol\'s masters (Persuade)', scene_3a, scene_3b, scene_3c, 'checkpoint']
+
+var scene_2a = ['> Bad move\n> The troll tears you apart limb from limb\n\nPress enter to return to last checkpoint', 'none', 'none', 'none']
+
+var scene_1c = ['> The troll grumbles, gets up, and flings the gate open.\n> Towering over you he bellows, "Why you wake Grol?!"\n\nPress a number:\n1. Attack!\n2. Talk to troll', scene_2a, scene_2b, 'none', 'checkpoint']
+
+var scene_1b = ['> The cell is cramped and empty.\n> There\'s nothing you can use here.\n\nPress a number:\n1. Check gate\n2. Attempt to wake up the troll', scene_1a, scene_1c, 'none']
+
+var scene_1a = ['> The gate is locked.\n\nPress a number:\n1. Look around the cell\n2. Attempt to wake up the troll', scene_1b, scene_1c, 'none', 'checkpoint']
+
+var scene_0 = ['> You find yourself in a cold dark cell.\n> A rock troll sleeps just outside the gate shaking the room with deafening snores.\n> What do you do?\n\nPress a number:\n1. Check gate\n2. Look around the cell\n3. Attempt to wake up the troll', scene_1a, scene_1b, scene_1c]
+
+var saveScene = scene_1a
 
 button.addEventListener('click', (event) => {
   event.preventDefault()
@@ -40,10 +59,7 @@ function newOptions(op1, op2, op3) {
     if (event.code == 'Digit1') {
       if (op1.length === 0) {
         clearText()
-        nextScene(currScene)
-        var newP = document.createElement('p')
-        newP.innerText = "> Try something else"
-        main.append(newP)
+        nextScene(saveScene)
       } else {
         clearText()
         nextScene(op1)
@@ -61,7 +77,27 @@ function newOptions(op1, op2, op3) {
 
 function nextScene(scene) {
   currScene = scene
-  console.log(currScene)
+  if (scene[4] === 'checkpoint') {
+    saveScene = scene
+  }
+  if (scene[1] === 'none') {
+    window.addEventListener('keyup', function enter(event) {
+      if (event.code === 'Enter') {
+        clearText()
+        nextScene(saveScene)
+      }
+      window.removeEventListener('keyup', enter)
+    })
+  }
+  if (scene[4] === 'continue') {
+    window.addEventListener('keyup', function enter(event) {
+      if (event.code === 'Enter') {
+        clearText()
+        nextScene(scene[1])
+      }
+      window.removeEventListener('keyup', enter)
+    })
+  }
   var newP = document.createElement('p')
   newP.innerText = scene[0]
   main.append(newP)
@@ -73,8 +109,5 @@ function clearText() {
 }
 
 function beginQuest() {
-  // display initial text
   nextScene(scene_0)
-  // look for user input
-  // continue with story
 }
